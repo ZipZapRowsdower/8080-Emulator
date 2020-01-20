acd3ee21698c96f475376a4c5b2e54d5c68b862c
Zap Rowsdower <ZipZapRowsdower@gmail.com>
2020-01-19 21:20:40 -0500

    Add Registers class and associated test fixture
    The ProcessorTest fixture has been renamed to ConditionCodesFixtures to
    avoid confusion.

--- src/include/processor.h
+++ src/include/processor.h
@@ -24,4 +24,18 @@ class ConditionCodes {
   uint8_t pad:3;
 };
 
+// All registers except the status register
+class Registers {
+ public:
+  uint8_t a;
+  uint8_t b;
+  uint8_t c;
+  uint8_t d;
+  uint8_t e;
+  uint8_t h;
+  uint8_t l;
+  uint16_t sp;
+  uint16_t pc;
+};
+
 #endif // PROCESSOR_H_
--- test/processor_test.cpp
+++ test/processor_test.cpp
@@ -10,23 +10,33 @@ const static uint8_t cy_2 = 1;
 const static uint8_t ac_2 = 1;
 const static uint8_t pad_2 = 1;
 
-struct ProcessorTest : testing::Test {
+struct ConditionCodesTest : testing::Test {
   ConditionCodes* cc_1;
   ConditionCodes* cc_2;
   ConditionCodes* cc_3;
 
-  ProcessorTest()
-  {
+  ConditionCodesTest() {
     cc_1 = new ConditionCodes;
     cc_2 = new ConditionCodes(z_2, s_2, p_2, cy_2, ac_2, pad_2);
     cc_3 = new ConditionCodes;
   }
 
-  virtual ~ProcessorTest()
-  {
+  virtual ~ConditionCodesTest() {
     delete cc_1;
     delete cc_2;
     delete cc_3;
+  }
+};
+
+struct RegistersTest : testing::Test {
+  Registers* registers_1;
+
+  RegistersTest() {
+    registers_1 = new Registers;
+  }
+
+  virtual ~RegistersTest() {
+    delete registers_1;
   }
 };
 
@@ -39,7 +49,7 @@ struct ProcessorTest : testing::Test {
 //  \____/\___/|_| |_|___/\__|_|   \__,_|\___|\__\___/|_|  |___/ //
 ///////////////////////////////////////////////////////////////////
 
-TEST_F(ProcessorTest, DefaultConditionCodeInitialization) {
+TEST_F(ConditionCodesTest, DefaultConditionCodeInitialization) {
   EXPECT_EQ(0, cc_1->z);
   EXPECT_EQ(0, cc_1->s);
   EXPECT_EQ(0, cc_1->p);
@@ -48,7 +58,7 @@ TEST_F(ProcessorTest, DefaultConditionCodeInitialization) {
   EXPECT_EQ(0, cc_1->pad);
 }
 
-TEST_F(ProcessorTest, FullConditionCodeInitialization) {
+TEST_F(ConditionCodesTest, FullConditionCodeInitialization) {
   EXPECT_EQ(z_2, cc_2->z);
   EXPECT_EQ(s_2, cc_2->s);
   EXPECT_EQ(p_2, cc_2->p);
@@ -57,7 +67,7 @@ TEST_F(ProcessorTest, FullConditionCodeInitialization) {
   EXPECT_EQ(pad_2, cc_2->pad);
 }
 
-TEST_F(ProcessorTest, CopyConstructorInitialization) {
+TEST_F(ConditionCodesTest, CopyConstructorInitialization) {
   ConditionCodes* my_cc = new ConditionCodes(*cc_2);
 
   EXPECT_EQ(cc_2->z, my_cc->z);
@@ -70,13 +80,13 @@ TEST_F(ProcessorTest, CopyConstructorInitialization) {
   delete my_cc;
 }
 
-TEST_F(ProcessorTest, Clear) {
+TEST_F(ConditionCodesTest, Clear) {
   cc_2->Clear();
 
   EXPECT_EQ(true, cc_1->operator==(*cc_2));
 }
 
-TEST_F(ProcessorTest, IsClear) {
+TEST_F(ConditionCodesTest, IsClear) {
   EXPECT_EQ(true, cc_1->IsClear());
   EXPECT_EQ(false, cc_2->IsClear());
 }
@@ -91,7 +101,7 @@ TEST_F(ProcessorTest, IsClear) {
 //                    //
 ////////////////////////
 
-TEST_F(ProcessorTest, ToString) {
+TEST_F(ConditionCodesTest, ToString) {
   std::string expected = "        C=1, P=1, S=1, Z=1\n";
   std::string actual = cc_2->ToString();
   EXPECT_EQ(expected, actual);
@@ -108,7 +118,7 @@ TEST_F(ProcessorTest, ToString) {
 //       |_|                                      //
 ////////////////////////////////////////////////////
 
-TEST_F(ProcessorTest, StreamInsertionOverload) {
+TEST_F(ConditionCodesTest, StreamInsertionOverload) {
   std::string expected = "        C=1, P=1, S=1, Z=1\n";
 
   std::ostringstream buffer;
@@ -118,7 +128,7 @@ TEST_F(ProcessorTest, StreamInsertionOverload) {
   EXPECT_EQ(expected, actual);
 }
 
-TEST_F(ProcessorTest, EqualsOperatorOverload) {
+TEST_F(ConditionCodesTest, EqualsOperatorOverload) {
   *cc_3 = *cc_2;
 
   EXPECT_EQ(cc_3->z, cc_2->z);
@@ -129,7 +139,7 @@ TEST_F(ProcessorTest, EqualsOperatorOverload) {
   EXPECT_EQ(cc_3->pad, cc_2->pad);
 }
 
-TEST_F(ProcessorTest, EqualityOperatorOverload) {
+TEST_F(ConditionCodesTest, EqualityOperatorOverload) {
   *cc_3 = *cc_2;
 
   EXPECT_EQ(true, cc_1->operator==(*cc_1));
@@ -143,7 +153,7 @@ TEST_F(ProcessorTest, EqualityOperatorOverload) {
   EXPECT_EQ(true, cc_3->operator==(*cc_2));
 }
 
-TEST_F(ProcessorTest, NotEqualityOperatorOverload) {
+TEST_F(ConditionCodesTest, NotEqualityOperatorOverload) {
   *cc_3 = *cc_2;
 
   EXPECT_EQ(false, cc_1->operator!=(*cc_1));
